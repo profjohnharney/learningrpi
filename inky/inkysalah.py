feecb36823fd065e7961c346cdc155da91b62619
John Harney <john.harney@centre.edu>
2020-01-17 19:56:14 -0500

    forgot to change path

--- inky/inkysalah.py
+++ inky/inkysalah.py
@@ -6,6 +6,6 @@ from PIL import Image, ImageFont, ImageDraw
 inky_display = InkyPHAT("yellow")
 # inky_display.set_border(inky_display.WHITE)
 
-img = Image.open("/home/pi/inky.png")
+img = Image.open("salahmane.png")
 inky_display.set_image(img)
 inky_display.show()
